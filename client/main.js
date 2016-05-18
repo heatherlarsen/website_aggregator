32ba1ddaff46829e73adb446a5cff47d4a65e5c9
Heather Larsen <heather@heatherlarsen.net>
2016-05-17 20:47:50 -0700

    completed tasks 1-6

--- client/main.js
+++ client/main.js
@@ -7,14 +7,30 @@ Router.route('/', function () {
   this.render('navbar', {
     to: "navbar"
   });
-  this.render('website_form', {
+  this.render('website_list', {
   	to: "main"
   });
-  this.render('website_list', {
+  this.render('website_form', {
   	to: "secondary"
-  });;
+  });
 });
 
+Router.route('/website/:_id', function() {
+	this.render('navbar', {
+		to: "navbar"
+	});
+	this.render('website', {
+		to: "main",
+		data: function() {
+			return Websites.findOne({_id:this.params._id});
+		}
+	});
+});
+
+// accounts config
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+});
 
 /////
 // template helpers 
@@ -23,7 +39,13 @@ Router.route('/', function () {
 // helper function that returns all available websites
 Template.website_list.helpers({
 	websites:function(){
-		return Websites.find({});
+		return Websites.find({}, {sort:{rating:-1}});
+	}
+});
+
+Template.comment_list.helpers({
+	comments: function() {
+		return Comments.findOne({websiteId:this._id});
 	}
 });
 
@@ -34,40 +56,97 @@ Template.website_list.helpers({
 
 Template.website_item.events({
 	"click .js-upvote":function(event){
-		// example of how you can access the id for the website in the database
-		// (this is the data context for the template)
 		var website_id = this._id;
-		console.log("Up voting website with id "+website_id);
-		// put the code in here to add a vote to a website!
+
+		if (Meteor.user()) {
+			// get current rating for the website
+			var currRating = Websites.findOne({_id:website_id}).rating;
+			if (currRating === undefined || currRating === NaN) currRating = 0;
+			var rating = currRating + 1;
+			Websites.update({_id:website_id}, {$set: {rating: rating}});
+		} else {
+			$("#login_warning").modal('show');
+		}
 
 		return false;// prevent the button from reloading the page
 	}, 
 	"click .js-downvote":function(event){
-
-		// example of how you can access the id for the website in the database
-		// (this is the data context for the template)
 		var website_id = this._id;
-		console.log("Down voting website with id "+website_id);
-
-		// put the code in here to remove a vote from a website!
+		
+		if (Meteor.user()) {
+			// get current rating for the website
+			var currRating = Websites.findOne({_id:website_id}).rating;
+			if (currRating === undefined || currRating === NaN) currRating = 0;
+			var rating = currRating - 1;
+			Websites.update({_id:website_id}, {$set: {rating: rating}});
+		} else {
+			$("#login_warning").modal('show');
+		}
 
 		return false;// prevent the button from reloading the page
 	}
 })
 
 Template.website_form.events({
-	"click .js-toggle-website-form":function(event){
-		$("#website_form").toggle('slow');
-	}, 
+	'click .js-show-website-form': function(event) {
+        $("#website_add_form").modal('show');
+    }, 
+    "click .js-cancel-form": function(event) {
+    	$("#website_add_form").modal('hide');
+    },
 	"submit .js-save-website-form":function(event){
-
-		// here is an example of how to get the url out of the form:
 		var url = event.target.url.value;
-		console.log("The url they entered is: "+url);
-		
-		//  put your website saving code in here!	
+		var title = event.target.title.value;
+		var description = event.target.description.value;
+        
+        if (Meteor.user()) {
+    		Websites.insert({
+                url: url,
+                title: title, 
+                description: description,
+                createdOn: new Date(), 
+                createdBy: Meteor.user()._id
+            });
 
-		return false;// stop the form submit from reloading the page
+            $("#website_add_form").modal('hide');
+        } else {
+        	$("#website_add_form").modal('hide');
+        	$("#login_warning").modal('show');
+        }
 
+		return false; // stop the form submit from reloading the page
+
+	}, 
+	"click .js-close-warning": function(event) {
+		$("#login_warning").modal('hide');
+	}
+});
+
+Template.website.events({
+	'click .js-show-comment-form': function(event) {
+		$('#comment_add_form').modal('show');
+	}, 
+	'click .js-cancel-form': function(event) {
+		$('#comment_add_form').modal('hide');
+	}, 
+	'submit .js-save-comment-form': function(event) {
+		if (Meteor.user()) {
+			Comments.insert({
+				websiteId: this._id,
+				comment: event.target.comment.value,
+				createdOn: new Date(),
+				createdBy: Meteor.user()._id
+			});
+
+			$('#comment_add_form').modal('hide');
+		} else {
+			$('#comment_add_form').modal('hide');
+			$("#comment_login_warning").modal('show');
+		}
+
+		return false;
+	}, 
+	'click .js-close-comment-warning': function(event) {
+		$("#comment_login_warning").modal('hide');
 	}
 });
